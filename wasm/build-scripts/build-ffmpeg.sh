64e5af10ef09cb95009cbc24c4673585813e1b13
Siam Mridha <smridha@echo360.com>
2022-06-09 22:56:51 -0400

    only made memory changes

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -28,13 +28,13 @@ FLAGS=(
   -s EXIT_RUNTIME=1                             # exit runtime after execution
   -s MODULARIZE=1                               # use modularized version to be more flexible
   -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
-  -s EXPORTED_FUNCTIONS="$EXPORTED_FUNCTIONS"  # export main and proxy_main funcs
-  -s EXTRA_EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
+  -s EXPORTED_FUNCTIONS="[_main]"  # export main and proxy_main funcs
+  -s EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
   -s INITIAL_MEMORY=33554432                    # 32MB
   -s MAXIMUM_MEMORY=1073741824                  # 1GB
   -s ALLOW_MEMORY_GROWTH=1
-  --post-js wasm/src/post.js
   --pre-js wasm/src/pre.js
+  --post-js wasm/src/post.js
   $OPTIM_FLAGS
   ${EXTRA_FLAGS[@]}
 )
